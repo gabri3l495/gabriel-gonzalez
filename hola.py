1783c75c1b6d72ebb58848475e61772d9a795096
gabriel <dominios10k@gmail.com>
2025-04-15 11:57:04 -0400

    primer commit

---
+++ hola.py
@@ -0,0 +1,15 @@
+#declaracion de variables
+#nombre="link"
+#edad=64
+#ejemplo de concatenacion
+#print("hola", nombre, " y su edad es", edad)
+
+# print("ingrese su nombre")
+# nombre=input()
+# print ("ingrese los numeros")
+
+n1=int(input("ingrese un numero"))
+n2=int(input("ingrese un numero"))
+print ("el resultado de la suma es", n1+n2)
+
+
