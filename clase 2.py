6e5128a8d515950e7ad33dc83fefbfd618f3f268
gabriel <dominios10k@gmail.com>
2025-04-26 16:47:29 -0400

    clase 2

---
+++ clase 2.py
@@ -0,0 +1,38 @@
+# #tabla de multiplicar 
+# num=int(input("ingrese un numero"))
+# for i in range (1,11):
+#     print(num, "x" ,i, "=", num*i)
+
+
+# #decir nombre
+# nombre=("ingrese nombre")
+# for i in range(3):
+#     print("hola", nombre)
+
+
+# #pida al usuario 3 notas y sacar el promedio (for obligatorio)
+# cant=int(input("ingrese su nota"))
+# suma=0
+# for i in range(3):
+#     n1=int(input("ingrese su nota "))
+#     suma+=1
+# prom=suma/3
+# print("el promedio es", round(prom))
+
+
+# #ingresa cantida de x notas para sacar promedio
+# cant=int(input("ingrese cantidad de notas"))
+# suma=0
+# for i in range (cant):
+#     n1=int(input("ingrese su nota"))
+#     suma=suma+n1
+# prom=suma/cant
+# print("el promedio es", round(prom))
+
+
+# #suma de numeros 
+# cant=int(input("ingrese cantidad de numeros"))
+# suma=0
+# for i in range(cant):
+#     n1=int(input("ingrese los numeros"))
+# print("la suma de todos los numeros es", suma)
---
+++ clase3.py
@@ -0,0 +1,68 @@
+#for
+#designar 2 dandidatos, preguntar cantida de votantes por cada votante preguntar por quien votara 
+#contar los votos y mostrar los resultados y definir el ganador 
+# c1="coyote"
+# c2="porky"
+# cantvotos1=0
+# cantvotos2=0
+# cantv=int(input("ingrese la cantidad de votantes :"))
+# for i in range(cantv):
+#     # print("por quien votara: 1.-" , c1, " , 2.-" , c2)
+#     print(f"por quien votara?: .-{c1}, 2.- {c2}")
+#     voto=input()
+#     if voto=="1" :
+#          print(f"usted voto por {c1}")
+#          cantvotos1=cantvotos1+1
+#     else:
+#          print(f"usted voto por {c2}")
+#          cantvotos2=cantvotos2+1
+# print(f"la cantidad de votos de {c1} es {cantvotos1}")
+# print(f"la cantidad de votos de {c2} es {cantvotos2}")
+# if cantvotos1>cantvotos2:
+#     print(f"gano {c1}")
+# elif cantvotos1<cantvotos2:
+#     print(f"gano {c2}")
+# else:
+#     print("votacion empatada")
+
+
+#cant de notas y promediar
+# cant=int(input("cantidad de notas"))
+# suma=0
+# for i in range(cant):
+#     print("ingrese nota" ,i +1)
+#     nota=float(input())
+#     if nots>=4:
+#          notas_azules=notas_azules+1
+#     suma=suma+nota
+# prom=suma/cant
+# print("su promedio es ", round, prom, 1)
+# print(f"obtuvo(notas_azules) notas sobre 4 ")
+# if prom>=4:
+#     print("el alumno aprobo")
+# else:
+#     print("el alumno reprobo")
+
+
+#recordatorio
+# nombre=input("ingrese su nombre")
+# edad=input("ingrese su edad")
+# print("hola", nombre, "su edad es", edad)
+
+
+#pida al usuario una frase y cuente los caracteres
+#ejemplo
+# nombre=input("ingrese su nombre ")
+# for i in nombre:
+#     print(i)
+
+#real
+# frase=int(input("ingrese una frase"))
+# cont=0
+# for i in frase:
+#       cont=cont+1
+# print("la cantidad de caracteres son", cont)
+
+# #otra forma de hacerlo
+# cont=len(frase)
+# print("la cantidad de caracteres son", cont)
