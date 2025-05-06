4c830918772ac1d21e4b897828402d364bb1df41
gabriel <dominios10k@gmail.com>
2025-05-06 13:22:02 -0400

    ejerc

---
+++ ejerc.py
@@ -0,0 +1,90 @@
+'''
+puntaje de credito de una persona
+'''
+# ingreso=int(input("ingrese sus ingresos, los cuales deben ser mayores a 500.000 -> "))
+# if ingreso>=500 and ingreso<1000000:
+#     print("sus ingresos corresponderian a 300.000")
+#     ing=300000
+# elif ingreso>=1000000 and ingreso<=1500000:
+#     print("sus ingresos corresponderian a 650.000")
+#     ing=650000
+# elif ingreso>1500001:
+#     print("sus ingresos corresponderian a 1.000.000")
+#     ing=1000000
+
+# educacion=int(input('''
+#                     nivel de educacion
+#                     1-basico
+#                     2-medio
+#                     3-superior
+#                     '''))
+# if educacion==1:
+#     ed=1
+# elif educacion==2:
+#     ed=1.3
+# elif educacion==3:
+#     ed=1.5
+
+
+# nacio=int(input('''
+#             ingrese su nacionalidad
+#             1-chilena
+#             2-extranjera
+#             '''))
+# if nacio==1:
+#     extra=300000
+#     total=ing*ed+extra
+#     print("su puntaje de credito corresponde a $", round(total))
+
+# elif nacio==2:
+#     total=ing*ed
+#     print("su puntaje de credito corresponde a $",round(total))
+
+
+
+
+
+
+
+
+
+'''desayuno'''
+# import time         #esto es para que funsione el time de abajo y este no se encentra el otro no funsionaria 
+
+# pan=250
+# while pan!=0:       #mintras pan no sea 0, el programa seguira funcionando 
+#     print("ñam")
+#     pan-=50         #50 es la cantidad de gramos que consume
+#     time.sleep(2)   #esto hace que espere 2 segundo para que continue
+
+
+'''chocolateeee'''
+# chocolate=1
+# if chocolate !=0:
+#     print("pude comprar chocolateee")
+# else:
+#     print("no puede comprar chocolateee")
+
+
+# ▄ #alt+220(del teclado numerico :3)
+# import random
+# from random import randint
+# print("el primer numero debe ser menor al segundo")
+# n1=int(input("ingrese su puto numero"))
+# n2=int(input("ingrese su puto numero"))
+# while n1>n2:
+#     print("el segundo numero no puede ser menor")
+#     n2=int(input("ingrese el numero 2 nuevamente"))
+# numa=random.randint(n1,n2)
+# na=randint(n1,n2)
+# print(" ▄ " *numa)
+
+    
+    
+
+      
+
+    
+
+
+
